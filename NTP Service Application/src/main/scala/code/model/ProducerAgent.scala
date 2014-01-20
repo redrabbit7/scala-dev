28b8a8c2139511752d4e46681994bc42a60d0ab9
redrabbit7 <sahithi.reddigari@gmail.com>
2014-01-20 16:04:32 -0500

    Add Time message to system scheduler in ProducerAgent

--- NTP Service Application/src/main/scala/code/model/ProducerAgent.scala
+++ NTP Service Application/src/main/scala/code/model/ProducerAgent.scala
@@ -8,18 +8,26 @@ import akka.routing.Broadcast
 import akka.actor.ActorRef
 import akka.routing.DefaultResizer
 import akka.routing.Router
+import scala.concurrent.duration._
+import akka.actor.Cancellable
+import java.util.concurrent.TimeUnit
+
 
 class ProducerAgent(nrOfConsumers: Int) extends Actor {
-    
+    val system = akka.actor.ActorSystem("system")
     var consumers = Set.empty[ActorRef]
-    sendTime
-	checkConsumerActivity
+    var count = Int
+    import system.dispatcher
     
 	//send current time every second to all live consumers
     def sendTime = {
-      consumers foreach (_ ! Messages.Time(System.currentTimeMillis))
-      Thread sleep 1000
+      while(1==1){
+	      consumers foreach (_ ! Messages.Time(System.currentTimeMillis))
+	      Thread sleep 1000
+      }
     }
+    
+
 
     //check each Consumer every 10 seconds for activity
     def checkConsumerActivity = {
@@ -36,7 +44,17 @@ class ProducerAgent(nrOfConsumers: Int) extends Actor {
         
         println("Registered consumer "+identity)
 	    consumers += consumer
+	    
+	    if (consumers.size == nrOfConsumers){
+	      println("All consumers are registered...")
+	      //consumers foreach (_ ! Messages.Time(System.currentTimeMillis))
+	      
+	      system.scheduler.scheduleOnce(1000 milliseconds, consumer, Messages.Time(System.currentTimeMillis))
+	      //checkConsumerActivity
+	    }
+	    
     }
+    
     
     
   }
