3eed56c330f394b8db52cf61d09591a6682bbebc
redrabbit7 <sahithi.reddigari@gmail.com>
2014-01-20 16:04:32 -0500

    use system scheduler for Registration and KeepAlive messages from consumer to producer

--- NTP Service Application/src/main/scala/code/model/Consumer.scala
+++ NTP Service Application/src/main/scala/code/model/Consumer.scala
@@ -3,25 +3,24 @@ package code.model
 
 import akka.actor.Actor
 import akka.actor.ActorRef
+import scala.concurrent.duration._
+import akka.actor.Cancellable
 
   class Consumer(producerAgent: ActorRef, maxMessagesSent: Int, ID: Int) extends Actor {
     val TIME_INTERVAL = 5000
+    import system.dispatcher
+    val system = akka.actor.ActorSystem("system")
     
     registerSelf
     keepMeAlive
 
     def registerSelf = {
-	  producerAgent ! Messages.Register(self,ID)
-	  Thread.sleep(TIME_INTERVAL)
+	  system.scheduler.scheduleOnce(TIME_INTERVAL milliseconds, producerAgent, Messages.Register(self,ID))
 	}
     
     def keepMeAlive = {
-      
-      for(a <- 1 to maxMessagesSent){
-    		producerAgent ! Messages.KeepAlive(ID)
-    		Thread.sleep(TIME_INTERVAL)
-      }
-      //context.stop(self)
+      system.scheduler.schedule(0 milliseconds,TIME_INTERVAL milliseconds, producerAgent, Messages.KeepAlive(ID))
+
     }
 
     def receive = {
