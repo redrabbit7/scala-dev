189afce4b6387cac0ede3e531961bdc8bbbe14cc
redrabbit7 <sahithi.reddigari@gmail.com>
2014-01-21 03:09:58 -0500

    Add main method

--- NTP Service Application/src/test/scala/RunNTPService.scala
+++ NTP Service Application/src/test/scala/RunNTPService.scala
@@ -3,24 +3,25 @@ import akka.actor.Props
 import code.model.Consumer
 import code.model.ProducerAgent
 
-object NTPService extends App {
+object NTPService {
 
-  //read number of consumers from console
-  println("Enter number of consumers > ");
-  val numConsumers = readInt
+  def main(args: Array[String]) {
+    //read number of consumers from console
+    println("Enter number of consumers > ");
+    val numConsumers = readInt
 
-  val system = ActorSystem("System")
-  val producerAgent = system.actorOf(Props(new ProducerAgent(numConsumers)), name = "producerAgent")
+    val system = ActorSystem("System")
+    val producerAgent = system.actorOf(Props(new ProducerAgent(numConsumers)), name = "producerAgent")
 
-  for (a <- 1 to numConsumers) {
-    //generate random MAX number of messages for the next consumer
-    val rnd = new scala.util.Random
-    val range = 1 to 12
-    val MAX_MESSAGES_SENT = range(rnd.nextInt(1 to 12 length))
+    for (a <- 1 to numConsumers) {
+      //generate random MAX number of messages for the next consumer
+      val rnd = new scala.util.Random
+      val range = 1 to 12
+      val MAX_MESSAGES_SENT = range(rnd.nextInt(1 to 12 length))
 
-    //create actor with ID of a
-    val consumer = system.actorOf(Props(new Consumer(producerAgent, MAX_MESSAGES_SENT, a)))
-
+      //create actor with ID of a
+      val consumer = system.actorOf(Props(new Consumer(producerAgent, MAX_MESSAGES_SENT, a)))
+    }
   }
 
 } 
